8fa85049c8e1662cddf7f1508a33d88993efb5ae
mn-manuprasad <mnmanuprasad02@gmail.com>
2021-10-31 21:38:35 +0530

    Binary Search

--- 02.Binary Search/13.py
+++ 02.Binary Search/13.py
@@ -9,12 +9,14 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         start = 1
         end = x
+        if x==0:
+            return 0
         while(start<=end):
             mid = start + (end-start)//2
             if mid*mid>x:
                 end  = mid-1
             elif mid*mid<x:
-                start = mid
+                start = mid + 1
             else:
                 return mid
         return end
